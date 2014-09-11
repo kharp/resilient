b31c72593d68e55db986b72a34d70aee05f3b02f
Katie <katie@spifferoni.com>
2014-09-10 18:27:06 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Athingwithfeathers::Application.config.secret_key_base = '02ac22a949bb2ffb33da26e30fcca3c45ea3c41d094ce6700d2c76318ba7c8005315785cf275b081e877838f2d33e88385c5eaebb06902055cadcf1ef4349464'

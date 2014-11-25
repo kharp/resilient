9607cadc1c67f9d9f4475a4dbd956c1a6c3a51be
JXDX <joshjadavies@gmail.com>
2014-11-24 20:10:56 -0800

    change access to keys to environment variables for heroku

--- config/initializers/setup_paperclip.rb
+++ config/initializers/setup_paperclip.rb
@@ -1,8 +1,8 @@
 Paperclip::Attachment.default_options.merge!({
  storage: :s3,
  s3_credentials: {
-   access_key_id: 'AKIAIQU6LKXE737EURCQ',
-   secret_access_key: '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',
-   bucket:        'resilient-bucket'
+   access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+   secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+   bucket:        ENV['S3_BUCKET_NAME']
  }
 });

f72aab58da47c17880960a8efdb58a6771485960
JXDX <joshjadavies@gmail.com>
2014-11-24 20:04:19 -0800

    setup S3 access keys

--- config/initializers/setup_paperclip.rb
+++ config/initializers/setup_paperclip.rb
@@ -1,8 +1,8 @@
 Paperclip::Attachment.default_options.merge!({
  storage: :s3,
  s3_credentials: {
-   access_key_id: ENV['katie@resilientapp.com'],
-   secret_access_key: ENV['developeR2014'],
+   access_key_id: 'AKIAIQU6LKXE737EURCQ',
+   secret_access_key: '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',
    bucket:        'resilient-bucket'
  }
 });

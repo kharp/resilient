52483688186a91fec6b7f63ed981e3e71a2fa9ec
JXDX <joshjadavies@gmail.com>
2014-11-27 21:55:23 -0800

    add user_id to channels

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141128052647) do
+ActiveRecord::Schema.define(version: 20141128055247) do
 
   create_table "categories", force: true do |t|
     t.string   "category"
@@ -31,7 +31,10 @@ ActiveRecord::Schema.define(version: 20141128052647) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "channels", ["user_id"], name: "index_channels_on_user_id"
 
   create_table "comments", force: true do |t|
     t.string   "user_name"

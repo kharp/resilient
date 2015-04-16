b5d13fedd11a108812e9c296085ae0b33c03a239
ldrohan <drohanluke@gmail.com>
2015-04-16 14:16:43 -0700

    fixed destroy pin method bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
 
+ 
+
  # Prevent CSRF attacks by raising an exception.
  # For APIs, you may want to use :null_session instead.
  before_filter :configure_permitted_parameters, if: :devise_controller?
